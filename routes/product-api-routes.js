5fd6906695640e5a46d91180744dcce799dc2e8f
Michael Dillie <michaeldillie@gmail.com>
2018-06-12 09:00:44 -0500

    We had to create a new repo with the safe code on it. This will now be our new repo

---
+++ routes/product-api-routes.js
@@ -0,0 +1,28 @@
+var db = require("../models");
+var passport = require("../config/passport");
+
+module.exports = function(app) {
+
+  app.get("/api/products", function(req, res) {
+    currentUser = req.user;
+    db.Product.findAll({}).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  app.post("/api/products", function(req, res) {
+
+    db.Product.create({
+      UserId: req.user.id,
+      vendorName: req.body.vendorName,
+      productName: req.body.productName,
+      description: req.body.description
+    }).then(function(result) {
+      res.json(result);
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+    });
+  });
+
+}
